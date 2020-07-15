da7f5b7050edfbf3341c6c3af940781e7df37777
podilon <podilon@gmail.com>
2020-07-15 01:03:15 -0300

    Conceitos básicos de JavaScript

---
+++ js/main.js
@@ -0,0 +1,82 @@
+function grato() {
+    document.getElementById("msg").innerHTML = "Valeu Mano!!!";
+}
+
+function opcoes(elemento) {
+    console.log(elemento.value)
+
+}
+
+
+/*function soma(n1, n2) {
+    return n1 + n2;
+}
+
+alert(soma(10, 5));
+
+
+
+ function validarIdade(idade) {
+    var valido;
+    if (idade >= 18) {
+        valido = true;
+    } else {
+        valido = false;
+    }
+    return valido;
+}
+
+var idade = prompt("Qual sua idade?");
+
+alert(validarIdade(idade));
+console.log(validarIdade(idade));
+
+
+
+
+
+var dia = new Date();
+alert(dia.getDate);
+alert(dia.getHours);
+
+
+
+
+
+var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+
+};
+
+
+
+var nome = "Paulo Odilon";
+alert('Bem Vindo, ' + nome);
+console.log(nome);
+var lista = ["maça", "pera", "laranja"]
+lista.pop("laranja");
+lista.push("uva");
+
+console.log(lista);
+console.log(lista.length);
+console.log(lista.sort());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+var cadastro = { nome: "Paulo Odilon", Idade: 41 };
+var cadastro = [{ nome: "Paulo Odilon", Idade: 41 }, { nome: "Soyane", Idade: 28 }];
+console.log(cadastro);
+
+var idade = prompt("Qual sua Idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}
+
+count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+};*/
